bd646dbc386a61d08f45961179ec441f30896b44
latiffurkandemir <latiffurkan.demir@gmail.com>
2025-01-06 23:57:51 +0300

    Add BaseEntity for common auditing and ID fields

--- src/main/java/com/todoapp/todo_api/ToDoApiApplication.java
+++ src/main/java/com/todoapp/todo_api/ToDoApiApplication.java
@@ -2,12 +2,13 @@ package com.todoapp.todo_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ToDoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoApiApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/todoapp/todo_api/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.todoapp.todo_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString
+public abstract class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false, nullable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false, nullable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false, nullable = false)
+    private String updatedBy;
+
+}
